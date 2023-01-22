fdb4b49b2206d2ca9d46e073e49e24c99bc68475
jambrose0 <jambrose0518@gmail.com>
2023-01-21 21:03:52 -0500

    initial registration page and styling completed

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-// import "./style.scss"
-
+import add from '../img/add.png'
 export const Register = () => {
   return (
     <div className='formContainer'>
@@ -12,7 +11,8 @@ export const Register = () => {
         <input type="text" placeholder='username' />
         <input type="email"  placeholder='email'/>
         <input type="password" placeholder='password' />
-        <input type="file" />
+        <input style={{display:"none"}} type="file" id='file'/>
+        <label htmlFor='file'><img src={add} alt="" /><span>Add a Profile Pic</span></label>
      <button>Sign Up</button>
      <p>Do you already have an account? Login</p>
     </form>
